0fff53ee18053c30d35c33df59275ed278f2e01f
Andrew Tram <MyMBP@Andrews-MacBook-Pro.local>
2017-03-13 19:22:32 -1000

    finished problem 15

--- functions.js
+++ functions.js
@@ -213,6 +213,14 @@ maximum(3, 2);
  * @return {boolean} the number is even
  */
 
+function isEven(n){
+	if(n % 2 === 0){
+		return true;
+	}else{
+		return false;
+	}
+}
+isEven(3);
 
 /**
  * Returns true if `n` is odd.
